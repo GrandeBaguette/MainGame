dcf63cdefcfb221614c90914edc30d091abca995
ParkourManiac <Daniel.kull1@gmail.com>
2017-09-19 11:12:36 +0200

    Began working on Fps script

---
+++ Apocalytic survival game/Assets/Scripts/FpsCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Add "Cursor.lockState" when game starts and Cursor.lockState to none when esc is pressed
+
+public class FpsCamera : MonoBehaviour {
+
+    void Update () {
+        float mouseInputX = Input.GetAxis("Mouse X");
+        float mouseInputY = Input.GetAxis("Mouse Y");
+
+
+    }
+}
